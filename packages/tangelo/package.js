bb66134a3f8007ec8744976d0c607e4ce337e938
Amy Slagle <slagle@ehatek.org>
2014-04-10 10:20:38 -0400

    Add a tangelo graph.

---
+++ packages/tangelo/package.js
@@ -0,0 +1,15 @@
+Package.describe({
+  summary: 'Tangelo packaged for Meteor'
+});
+
+Package.on_use(function (api) {
+  api.use('jquery', 'client');
+  api.use('d3', 'client');
+
+  api.add_files('jquery-ui.min.js', 'client');
+  api.add_files('autobahn.min.js', 'client');
+  api.add_files('vtkweb-all.min.js', 'client');
+  api.add_files('tangelo.js', 'client');
+
+  api.export('tangelo', 'client');
+});
