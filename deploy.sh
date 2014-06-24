8ad900e3d1c4b595f34d4a9550d6eb2f278a30e4
Nathan <nabreit@gmail.com>
2014-06-24 06:53:16 +0000

    Removing old bundle to prevent rebundling errors

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+# Remove the old bundle because extraneous files in the app dir can cause errors
+# when rebundling.
+rm -rf bundle
 mrt update
 mrt bundle bundle.tgz
 tar -zxvf bundle.tgz
-# Reinstall the fibers module (why?)
+# Reinstall the fibers module because it is a native package.
+# See: http://stackoverflow.com/questions/17606340/how-to-deploy-a-meteor-application-to-my-own-server
 cd bundle/programs/server/node_modules
 rm -rf fibers
 npm install fibers
 cd ../../../..
 # Restart the forever server process
 forever stop bundle/main.js || echo "no forever running"
-# using port 3001 because port 80 only works if running as root
+# Using port 3001 because port 80 only works if running as root
 PORT=3001 MONGO_URL=mongodb://localhost:27017/diagnosis ROOT_URL=http://grits-dev.ecohealth.io forever -a -o output.log -e error.log start bundle/main.js
