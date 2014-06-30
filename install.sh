c6ae0c41b22c3bc51e2cf3c449be2e4c355a6137
Amy Slagle <slagle@ehatek.org>
2014-06-30 14:28:37 -0400

    Remove install steps moved to other scripts.

--- install.sh
+++ install.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# These instructions were tested on a Ubuntu 14.04 LTS 64bit AWS Instance.
-
-sudo apt-get update
-sudo apt-get install -y git
-
-git clone https://$GIT_USER:$GIT_PASSWORD@github.com/ecohealthalliance/diagnostic-dashboard.git
-
+cd ~
 # Install node.js and meteorite:
 
 sudo apt-get install -y python-software-properties python g++ make
@@ -19,15 +13,8 @@ sudo npm install -g meteorite forever
 sudo rm -rf tmp
 sudo rm -rf .npm
 cd ~/diagnostic-dashboard
-git checkout setup-instructions
 
 # Deploy or redeploy the server:
 
 chmod 700 deploy.sh
 ./deploy.sh
-
-#By default meteor will run on port 3001. Run the Apache set-up script to put
-#proxy the server on port 80:
-
-chmod 700 apache_setup.sh
-./apache_setup.sh
