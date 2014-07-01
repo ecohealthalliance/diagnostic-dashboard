3ca46b75e23518111c8e329ebf2d142cc573d243
Amy Slagle <slagle@ehatek.org>
2014-07-01 14:03:17 -0400

    Remove install steps moved to grits-deploy-scripts.

--- install.sh
+++ install.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 cd ~
-# Install node.js and meteorite:
+# Install meteor and meteorite:
 
-sudo apt-get install -y python-software-properties python g++ make
-sudo add-apt-repository ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install -y nodejs
 curl https://install.meteor.com/ | sh
 sudo npm install -g meteorite forever
 # ~/tmp is an npm issue that will be fixed in the next release (https://github.com/npm/npm/issues/3470)
@@ -15,6 +11,4 @@ sudo rm -rf .npm
 cd ~/diagnostic-dashboard
 
 # Deploy or redeploy the server:
-
-chmod 700 deploy.sh
 ./deploy.sh
