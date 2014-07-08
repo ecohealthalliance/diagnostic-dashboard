68110c89635c14aeab3bfa6f09c787b54445a0e2
Jonathan Beezley <jonathan.beezley@kitware.com>
2014-07-08 20:17:19 +0000

    changing timeline to a histogram

--- client/controllers/timelineSimple.js
+++ client/controllers/timelineSimple.js
@@ -3,8 +3,7 @@
 (function ($, d3) {
     'use strict';
 
-    var nBins = 250,
-        radius = 10/nBins,
+    var nBins = 50,
         margin = {
             top: 25,
             left: 30,
@@ -23,85 +22,42 @@
             .attr('dx', '0.5em')
             .attr('dy', '0.1em');
     }
-
-    function binData(data, start, end) {
-        var bins = [], 
-            s = start.valueOf(),
-            e = end.valueOf(),
-            dt = (e - s)/(nBins - 1),
-            i, j, d;
-        bins.length = 100;
-        for (i = 0; i < nBins; i++) {
-            bins[i] = {
-                time: s + i * dt,
-                value: 0
-            };
-        }
-        for (i = 0; i < data.length; i++) {
-            d = data[i];
-            if (d.date) {
-                j = Math.floor((d.date.valueOf() - s) / dt);
-                if (j >= 0 && j < nBins) {
-                    bins[j].value += 1;
-                }
-            }
-        }
-        tangelo.data.smooth({
-            data: bins,
-            radius: radius,
-            kernel: 'gaussian',
-            absolute: false,
-            set: function (v, d) { d.value = v; },
-            sorted: true,
-            x: { field: 'time' },
-            y: { field: 'value' }
-        });
-        return bins;
-    }
     
     Template.timelineSimple.rendered = function () {
         var data = [];
         if (!this.initialized) {
             $(node).on('datachanged', function (evt, args) {
+                data = [];
                 if (!args.data.length) {
                     return;
                 }
-                var start = null, end = null;
                 args.data.forEach(function (d) {
                     if (!d.date) {
                         return;
                     }
-                    if (!start || d.date < start) {
-                        start = d.date;
-                    }
-                    if (!end || d.date > end) {
-                        end = d.date;
-                    }
+                    d.value = d.date.valueOf();
+                    data.push(d);
                 });
-                if (start && end) {
-                    data = binData(args.data, start, end);
-                } else {
-                    data = [];
-                }
-                $(node).timeline({
+                $(node).histogram({
                     data: data,
-                    x: { field: 'time' },
-                    y: { field: 'value' },
+                    x: { field: 'value' },
                     transition: 250,
                     margin: margin,
                     xTicks: xTicks,
-                    yTicks: yTicks
+                    yTicks: yTicks,
+                    xScale: d3.time.scale(),
+                    nBins: nBins
                 });
                 applyAxisStyle();
             }).on('resizeApp', function (evt, obj) {
-                $(node).timeline({
+                $(node).histogram({
                     data: data,
-                    x: { field: 'time' },
-                    y: { field: 'value' },
+                    x: { field: 'value' },
                     transition: 0,
                     margin: margin,
                     width: obj.width,
-                    height: obj.height
+                    height: obj.height,
+                    nBins: nBins
                 });
                 applyAxisStyle();
             });
